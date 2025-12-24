6d7eac9e7395bf0860661c482d75cc989ba5b5fe
Leanna Bucio <a02440198@usu.edu>
2025-12-23 20:28:49 -0700

    updated to fit new user manual and to implement user control over variables (The forecast specification is still hardcoded in)

--- src/get_data.py
+++ src/get_data.py
@@ -3,11 +3,11 @@ import requests
 url = "https://api.open-meteo.com/v1/forecast"
 
 # defaults are for Salt Lake City
-def get_data(latitude=40.746216, longitude=-111.905396):
+def get_data(variables="sunrise,sunset", latitude=40.746216, longitude=-111.905396):
     params = {
         "latitude" : latitude,
         "longitude": longitude,
-        "daily"    : "sunrise,sunset",
+        "daily"    : variables,
         "timezone" : "auto"
     }
     
